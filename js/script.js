0766a6d4ea6a7aadcb35077e5dd053516c5c48d8
Daniel Hoyos <danielhoyos350@gmail.com>
2017-06-23 09:22:34 -0500

    (V. 1.0.9) Agregación de funcionalidad a los botones y mejorando estilo html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-$(document).ready(function(){
-    $(".button-scroll").click(function(){
-        console.log("Iniciamos a programar con Js y JQuery");
+$(document).ready(function () {
+    $(".button-scroll").click(function () {
+        $top_scroll = $("footer").position().top;
+        moveScroll($top_scroll, 5000);
+    });
+    $(".nosotros").click(function () {
+        $top_scroll = $(".sub-header").position().top;
+        moveScroll($top_scroll, 500);
     });
 });
+
+function moveScroll($top_scroll, $seg) {
+    $('body, html').animate({
+        scrollTop: $top_scroll
+    }, $seg);
+}
