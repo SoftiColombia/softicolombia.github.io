8db1f245b422922123142236fc05746490e564d2
Daniel Hoyos <danielhoyos350@gmail.com>
2017-06-26 18:02:49 -0500

    (V. 1.0.10) Creación de la plantilla de servicios y contacto completa

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 $(document).ready(function () {
+    if(localStorage.getItem("move")){
+        $top_scroll = $(".sub-header").position().top;
+        moveScroll($top_scroll, 500);
+        localStorage.removeItem("move");
+    }
+    
     $(".button-scroll").click(function () {
         $top_scroll = $("footer").position().top;
         moveScroll($top_scroll, 5000);
     });
+    
     $(".nosotros").click(function () {
-        $top_scroll = $(".sub-header").position().top;
-        moveScroll($top_scroll, 500);
+        if ($(".sub-header").length) {
+            $top_scroll = $(".sub-header").position().top;
+            moveScroll($top_scroll, 500);
+        } else {
+            localStorage.setItem("move",true);
+            window.location.href = "index.html";
+        }
+    });
+    
+    $(".button-contacto").click(function(){
+        window.location.href = "contacto.html";
     });
 });
 
